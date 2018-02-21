b7d57a0a911f843b46de7bf5cc9a4af1b4df6458
Funncy <gywns6222@gmail.com>
2018-02-21 18:22:52 +0900

    index ,resource 추가 및 h2 database , jpa 연결

--- src/main/java/com/lnf/web/lnf/Controller/UserController.java
+++ src/main/java/com/lnf/web/lnf/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.lnf.web.lnf.Controller;
 
 import com.lnf.web.lnf.Data.User;
+import com.lnf.web.lnf.Data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +14,21 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    List<User> users = new ArrayList<User>();
+
+
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping("/create")
     public String create(User user){
         System.out.print("user="+user);
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/list";
     }
 
     @GetMapping("/list")
     public String list(Model model){
-        model.addAttribute("users",users);
+        model.addAttribute("users", userRepository.findAll());
         return "list";
     }
 }
--- src/main/java/com/lnf/web/lnf/Data/User.java
+++ src/main/java/com/lnf/web/lnf/Data/User.java
@@ -1,7 +1,19 @@
 package com.lnf.web.lnf.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable=false)
     private String userId;
+
     private String name;
     private String email;
     private String password;
