b4bc85f0c8bfa9778fa507c095629b53ac65ecef
Funncy <gywns6222@gmail.com>
2018-02-21 15:40:43 +0900

    first commit

---
+++ src/main/java/com/lnf/web/lnf/LnfApplication.java
@@ -0,0 +1,12 @@
+package com.lnf.web.lnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnfApplication.class, args);
+	}
+}
